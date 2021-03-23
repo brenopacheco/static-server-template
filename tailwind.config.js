29d9f96e3a777ee8a7de3fee1f126a37f6b9f9ff
Breno Pacheco <brenoleonhardt@gmail.com>
2021-03-23 14:37:22 +0000

    added tailwind options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-    purge: {
-        enabled: process.env.NODE_ENV === 'production' ? true : false,
-        content: ['./dist/index.html']
+  purge: {
+    enabled: process.env.NODE_ENV === "production" ? true : false,
+    content: ["./dist/index.html"],
+  },
+  theme: {
+    colors: {
+      // Build your palette here
+      black: colors.black,
+      blue: colors.lightBlue,
+      current: "currentColor",
+      gray: colors.blueGray,
+      green: colors.teal,
+      indigo: colors.indigo,
+      pink: colors.pink,
+      purple: colors.purple,
+      red: colors.red,
+      transparent: "transparent",
+      white: colors.white,
+      yellow: colors.amber,
     },
-    theme: {},
-    variants: {},
-    plugins: [],
-}
+    fontFamily: {
+        roboto: ['"Roboto"'],
+        fira:   ['"Fira Sans"'],
+        code:   ['"Fira Code"']
+    },
+  },
+  variants: {},
+  plugins: [],
+};
